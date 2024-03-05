bf8069759667becc0c98d31328bc35bf055c1e07
M-Ali-Haider <mah01134212100@gmail.com>
2024-03-05 19:36:14 +0500

    adjusted font sizes

--- src/component/CrownHeading/index.jsx
+++ src/component/CrownHeading/index.jsx
@@ -1,9 +1,14 @@
 import styles from './style.module.css'
 import nextConfig from '../../../next.config'
 import Image from 'next/image'
+
+import { Bebas_Neue } from 'next/font/google'
+
+const bebas = Bebas_Neue({subsets:["latin"],weight:["400"]});
+
 export default function CrownHeading({isMobile,heading}) {
   return (
-    <div className={styles.crownHeading}>
+    <div className={`${bebas.className} ${styles.crownHeading}`}>
         <Image 
             className={styles.crown}
             src={`${nextConfig.basePath}/images/crownsvg.svg`}
