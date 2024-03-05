44783819df1b39e196457b2f3ea0b0b5b975e44a
M-Ali-Haider <mah01134212100@gmail.com>
2024-03-05 19:52:45 +0500

    _

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,10 @@
 import { Inter } from "next/font/google";
+import { Bebas_Neue } from "next/font/google";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const bebas = Bebas_Neue({ subsets:["latin"], weight:["400"],display:'swap',variable:'--bebasFont'})
 
 export const metadata = {
   title: "Play Website",
@@ -12,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${bebas.variable}`}>{children}</body>
     </html>
   );
 }
--- src/component/CrownHeading/index.jsx
+++ src/component/CrownHeading/index.jsx
@@ -2,13 +2,9 @@ import styles from './style.module.css'
 import nextConfig from '../../../next.config'
 import Image from 'next/image'
 
-import { Bebas_Neue } from 'next/font/google'
-
-const bebas = Bebas_Neue({subsets:["latin"],weight:["400"]});
-
 export default function CrownHeading({isMobile,heading}) {
   return (
-    <div className={`${bebas.className} ${styles.crownHeading}`}>
+    <div className={styles.crownHeading}>
         <Image 
             className={styles.crown}
             src={`${nextConfig.basePath}/images/crownsvg.svg`}
